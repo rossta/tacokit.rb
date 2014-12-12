748a7313ddac6d443791e8bbac01ebeddafac407
Ross Kaffenberger <rosskaff@gmail.com>
2014-12-11 19:07:58 -0500

    parse response for get user with faraday

--- lib/tacoshell/client.rb
+++ lib/tacoshell/client.rb
@@ -1,4 +1,7 @@
 require 'forwardable'
+require 'faraday'
+require 'faraday_middleware'
+require 'hashie/mash'
 
 module Tacoshell
   class Client
@@ -6,12 +9,36 @@ module Tacoshell
 
     def_delegators :configuration, *Configuration.keys
 
+    API_ENDPOINT = "https://api.trello.com"
+
     def initialize(options = {})
       self.configuration.options = options
     end
 
     def configuration
       @configuration ||= Configuration.new
+    end
+
+    def member(username)
+      get "/1/members/#{username}", { key: app_key }
+    end
+
+    def get(url, options = {})
+      request :get, url, options
+    end
+
+    def request(method, url, options)
+      response = connection.send(method, url, options)
+      response.body
+    end
+
+    def connection
+      @connection = Faraday.new(url: API_ENDPOINT) do |http|
+        http.response :mashify
+        http.response :json
+        http.response :logger
+        http.adapter Faraday.default_adapter
+      end
     end
 
   end
--- tacoshell.gemspec
+++ tacoshell.gemspec
@@ -18,6 +18,10 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "faraday"
+  spec.add_dependency "faraday_middleware"
+  spec.add_dependency "hashie"
+
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.1"

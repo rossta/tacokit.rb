48a97034c3a32e540ea72ad0ce73d37cf3e9e207
Ross Kaffenberger <rosskaff@gmail.com>
2014-12-14 14:02:06 -0500

    update card specs and cassettes

--- spec/tacokit/client/cards_spec.rb
+++ spec/tacokit/client/cards_spec.rb
@@ -5,12 +5,12 @@ describe Tacokit::Client::Cards do
   describe "#card", :vcr do
     it "returns a card by short link" do
       card = app_client.card('k7GHJj7Q')
-      expect(card.name).to eq 'Test Card 1'
+      expect(card.name).to eq 'Card 1'
     end
 
     it "returns a card by id" do
       card = app_client.card('548dd95c8ca25ac9d0d9ce71')
-      expect(card.name).to eq 'Test Card 1'
+      expect(card.name).to eq 'Card 1'
     end
   end
 

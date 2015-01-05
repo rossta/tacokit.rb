393102ee0c62253782517e82ee8ba62cbd2a4177
Ross Kaffenberger <rosskaff@gmail.com>
2015-01-05 10:55:02 -0500

    resolve removal of client for resource

--- spec/tacokit/resource_spec.rb
+++ spec/tacokit/resource_spec.rb
@@ -2,11 +2,9 @@ require 'spec_helper'
 
 module Tacokit
   describe Resource do
-    let(:client) { OpenStruct.new }
-
     describe "#initialize" do
       it "handles given attributes" do
-        res = Resource.new client, one: 'one', 'two' => :two
+        res = Resource.new one: 'one', 'two' => :two
 
         expect(res).to be_a(Resource)
         expect(res.one).to eq 'one'
@@ -14,7 +12,7 @@ module Tacokit
       end
 
       it "handles nested objects" do
-        res = Resource.new client, prefs: { user: { read: 'ftw' }}
+        res = Resource.new prefs: { user: { read: 'ftw' }}
 
         expect(res.prefs.user.read).to eq 'ftw'
         expect(res.prefs).to be_a(Resource)
@@ -22,7 +20,7 @@ module Tacokit
       end
 
       it "has nested collections" do
-        res = Resource.new client, labels: [{ color: 'blue' }]
+        res = Resource.new labels: [{ color: 'blue' }]
 
         expect(res.labels.size).to eq 1
 
@@ -32,17 +30,16 @@ module Tacokit
       end
 
       it "handles clashing keys" do
-        res = Resource.new client, client: 1, fields: 2
+        res = Resource.new client: 1, fields: 2
 
-        expect(res.client).to eq 1
         expect(res.fields).to eq 2
 
-        expect(res._fields).to include(:client, :fields)
+        expect(res._fields).to include(:fields)
         expect(res._fields.size).to eq(2)
       end
 
       it "has attribute predicates" do
-        res = Resource.new client, a: 1, b: true, c: nil, d: false
+        res = Resource.new a: 1, b: true, c: nil, d: false
 
         expect(res.a?).to be_truthy
         expect(res.b?).to be_truthy
@@ -51,7 +48,7 @@ module Tacokit
       end
 
       it "has attribute setters" do
-        res = Resource.new client, a: 1
+        res = Resource.new a: 1
 
         expect(res.a).to eq 1
         expect(res.key?(:a)).to be_truthy
@@ -62,20 +59,20 @@ module Tacokit
       end
 
       it "dynamically adds attr methods through getters" do
-        res = Resource.new client
+        res = Resource.new
 
         expect(res.respond_to?(:a)).to be_falsey
         expect(res.respond_to?(:a=)).to be_falsey
         expect(res.respond_to?(:a?)).to be_falsey
 
-        res = Resource.new client, a: 1
+        res = Resource.new a: 1
         expect(res.respond_to?(:a)).to be_truthy
         expect(res.respond_to?(:a=)).to be_truthy
         expect(res.respond_to?(:a?)).to be_truthy
       end
 
       it "has nillable attribute getters" do
-        res = Resource.new client, a: 1
+        res = Resource.new a: 1
 
         expect(res.key?(:b)).to be_falsey
         expect(res.respond_to?(:b)).to be_falsey
@@ -86,7 +83,7 @@ module Tacokit
       end
 
       it "dynamically adds attr methods through setters" do
-        res = Resource.new client, a: 1
+        res = Resource.new a: 1
 
         expect(res.key?(:b)).to be_falsey
         expect(res.respond_to?(:b)).to be_falsey
@@ -102,7 +99,7 @@ module Tacokit
       end
 
       it "is enumerable" do
-        res = Resource.new client, a: 1, b: 2
+        res = Resource.new a: 1, b: 2
 
         output = []
         res.each { |k,v| output << [k,v] }
@@ -111,13 +108,13 @@ module Tacokit
 
       describe "#attrs" do
         it "returns hash of attributes" do
-          res = Resource.new client, a: 1
+          res = Resource.new a: 1
 
           expect(res.to_attrs).to eq a: 1
         end
 
         it "returns nested attributes" do
-          res = Resource.new client, a: 1, b: [{ c: 2 }]
+          res = Resource.new a: 1, b: [{ c: 2 }]
 
           expect(res.b).to be_a(Array)
           expect(res.b.first).to be_a(Resource)
@@ -126,7 +123,7 @@ module Tacokit
         end
 
         it "responds to [] and []=" do
-          res = Resource.new client, a: 1
+          res = Resource.new a: 1
 
           expect(res.a).to eq 1
           expect(res[:a]).to eq 1
